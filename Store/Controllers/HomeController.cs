d1322d37d9756f353a6f690d940732b8666d1f7a
bzorufchi <b.zorufchi@gmail.com>
2023-11-28 17:48:37 +0330

    add view

--- Store/Controllers/HomeController.cs
+++ Store/Controllers/HomeController.cs
@@ -46,8 +46,15 @@ namespace Store.Controllers
         {
             return View();
         }
+        public IActionResult User()
+        {
+            return View();
+        }
 
-
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
